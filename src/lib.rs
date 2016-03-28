8a0ce04f2ce8e804dc722ea6b49c8c6796d6c129
Erik Johnston <erikj@jki.re>
2016-03-28 17:08:28 +0100

    Stuff

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ pub mod signedjson;
 pub mod ser;
 
 use rustc_serialize::base64;
+use rustc_serialize::base64::ToBase64;
+
+use sodiumoxide::crypto::sign;
 
 
 pub const UNPADDED_BASE64 : base64::Config = base64::Config {
@@ -38,56 +41,80 @@ struct SignedRequest<'a, T: serde::Serialize + 'a> {
 
 
 /// Compute the signature for an outgoing federation request
-pub fn sign_request_b64(
+pub fn sign_request(
     key: &signedjson::SigningKey,
     method: &str,
     uri: &str,
     origin: &str,
     destination: &str,
-    content: Option<&signedjson::Object>,
-) -> Result<String, signedjson::SigningJsonError> {
-    let req = SignedRequest {
+) -> Result<sign::Signature, signedjson::SigningJsonError> {
+    signedjson::get_sig_for_json(key, &SignedRequest {
+        method: method,
+        uri: uri,
+        origin: origin,
+        destination: destination,
+        content: None as Option<&u8>,  // As the option needs a type.
+    })
+}
+
+/// Compute the signature for an outgoing federation request
+pub fn sign_request_with_content<T: serde::Serialize>(
+    key: &signedjson::SigningKey,
+    method: &str,
+    uri: &str,
+    origin: &str,
+    destination: &str,
+    content: Option<&T>,
+) -> Result<sign::Signature, signedjson::SigningJsonError> {
+    signedjson::get_sig_for_json(key, &SignedRequest {
         method: method,
         uri: uri,
         origin: origin,
         destination: destination,
         content: content,
-    };
-
-    Ok(try!(signedjson::get_sig_for_json_b64(key, &req)))
+    })
 }
 
 
+
 /// Generate a Matrix Authorization header.
-pub fn generate_auth_header(
+pub fn generate_auth_header<T: serde::Serialize>(
     key: &signedjson::SigningKey,
     method: &str,
     uri: &str,
     origin: &str,
     destination: &str,
-    content: Option<&signedjson::Object>,
+    content: Option<&T>,
 ) -> Result<String, signedjson::SigningJsonError> {
-    let sig = try!(sign_request_b64(key, method, uri, origin, destination, content));
+    let sig = try!(sign_request_with_content(key, method, uri, origin, destination, content));
     Ok(format!(
         r#"X-Matrix origin={},key="{}",sig="{}""#,
-        &origin, key.key_id, sig,
+        &origin, key.key_id, sig.0.to_base64(UNPADDED_BASE64),
     ))
 }
 
-#[test]
-fn test_sign_request() {
-    let key = signedjson::SigningKey::from_seed(&[0u8; 32], "ed25519:test".to_string()).unwrap();
-    let sig = sign_request_b64(&key, "GET", "/", "jki.re", "matrix.org", None).unwrap();
-    assert_eq!(sig, "PmWN0xPpzEWR97AIQ8Q+/dysu7uZBPMnUzcK9rwqgae3t8LIDSdpg4moQ4qzdkwjqv7+JIcyeFumPeDFLvDxAw");
-}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rustc_serialize::base64::FromBase64;
+
+    #[test]
+    fn test_sign_request() {
+        let key = signedjson::SigningKey::from_seed(&[0u8; 32], "ed25519:test".to_string()).unwrap();
+        let sig = sign_request(&key, "GET", "/", "jki.re", "matrix.org").unwrap();
+        let expected = "PmWN0xPpzEWR97AIQ8Q+/dysu7uZBPMnUzcK9rwqgae3t8LIDSdpg4moQ4qzdkwjqv7+JIcyeFumPeDFLvDxAw".from_base64().unwrap();
+        assert_eq!(&sig[..], &expected[..]);
+    }
 
 
-#[test]
-fn test_auth_header() {
-    let key = signedjson::SigningKey::from_seed(&[0u8; 32], "ed25519:test".to_string()).unwrap();
-    let header = generate_auth_header(&key, "GET", "/", "jki.re", "matrix.org", None).unwrap();
-    assert_eq!(
-        header,
-        r#"X-Matrix origin=jki.re,key="ed25519:test",sig="PmWN0xPpzEWR97AIQ8Q+/dysu7uZBPMnUzcK9rwqgae3t8LIDSdpg4moQ4qzdkwjqv7+JIcyeFumPeDFLvDxAw""#
-    );
+    #[test]
+    fn test_auth_header() {
+        let key = signedjson::SigningKey::from_seed(&[0u8; 32], "ed25519:test".to_string()).unwrap();
+        let header = generate_auth_header::<u8>(&key, "GET", "/", "jki.re", "matrix.org", None).unwrap();
+        assert_eq!(
+            header,
+            r#"X-Matrix origin=jki.re,key="ed25519:test",sig="PmWN0xPpzEWR97AIQ8Q+/dysu7uZBPMnUzcK9rwqgae3t8LIDSdpg4moQ4qzdkwjqv7+JIcyeFumPeDFLvDxAw""#
+        );
+    }
 }
